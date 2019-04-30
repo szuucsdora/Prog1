ee3361bfea48773ea220da51bae546ac4a31907f
Szűcs Dóra <47795907+szuucsdora@users.noreply.github.com>
2019-04-30 21:11:10 +0200

    Update Beadando.py

--- Beadando.py
+++ Beadando.py
@@ -1,2 +1,19 @@
 #27. feladat
 import numpy as np
+m=np.random.randint(0,100,(5,5))
+min1 = 0
+for i in range(0,m.shape[0]):
+    print(i)
+
+    if i > min1:
+        min1=i
+        print( min1)
+min2 = 0
+for j in range(0,m.shape[1]):
+    print(j)
+
+    if i > min2:
+        min2 = i
+        print(min2)
+
+print(m)

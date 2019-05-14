8f0bd31a131d82f7918b65d7fa130974ba9f17a5
Szűcs Dóra <47795907+szuucsdora@users.noreply.github.com>
2019-05-14 21:15:40 +0200

    Update 27.py

--- 27.py
+++ 27.py
@@ -1,27 +1,19 @@
 #27. feladat
 import numpy as np
-#def sorbanlegnagyobb():
-M=5
-N=5
-m=np.random.randint(0,100,(N,M))
+#def oszlopbanlegnagyobb():
+sor=5
+oszlop=5
+m=np.random.randint(0,100,(sor,oszlop))
 print(m)
-#min = 0
-ossz=0
-for i in range(0,N):
-    #print(i)
 
-  #  if m[i,0] > min1:
-   #     min1=i
-    #    print( min1)
-#min2 = 0
-    #for j in range(0,i+1):
-    #print(j)
+legnagyobb=0
+s=0
+while s<=4:
 
-        #if m[i,0] > min:
-            #min = m[i,j]
-            #print(min)
-
-    for j in range(0,M):
-        ossz += m[i][0]
-print(ossz)
-
+    for j in range(oszlop):
+        if m[j,s] > legnagyobb:
+            legnagyobb=m[j,s]
+            #return legnagyobb
+    print(f"{legnagyobb} a legnagyobb az {s}. oszlopban")
+    s+=1
+    legnagyobb=0
